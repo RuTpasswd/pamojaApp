3d1b6ce7469dd049f8d0b16beb18b1c33be8a738
RKNyra <rk.nyra@gmail.com>
2019-09-03 13:36:29 +0300

    add a function to hide the register form upon submitting it

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
     $(".registerbutton").click(function() {
         $(".bg-popUp").show().css("display", "flex");
     });
-
+    $(".submitBtn").click(function() {
+        $(".bg-popUp").hide();
+    });
 });
