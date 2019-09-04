3437cb99e9532ff365a560b4db0ba23c4dd2c936
RKNyra <rk.nyra@gmail.com>
2019-09-04 11:57:31 +0300

    add a Login buttion and Login form for users who are already registered on PamojaApp

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
     $(".registerbutton").click(function() {
         $(".bg-popUp").show().css("display", "flex");
+
     });
-    $(".submitBtn").click(function() {
+
+    $(".loginbutton").click(function() {
+        $(".bg-popUpLogin").show().css("display", "flex");
+    });
+
+    $(".submitBtn").on("click", function(e) {
+        e.preventDefault();
+        $("input").attr('required', true);
         $(".bg-popUp").hide();
+        $(".bg-popUpLogin").hide();
     });
 });
